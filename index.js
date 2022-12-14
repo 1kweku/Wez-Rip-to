9d66db12d015adff0caace549688276f1ba6e557
1kweku <kwekuplusp@gmail.com>
2022-12-14 11:55:11 -0500

    fetch not working

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const searchBtn = document.getElementById("search-Btn");
-const searchForm = document.getElementById("search-form");
+const searchBtn = document.getElementById("search-btn");
+//const searchForm = document.getElementById("search-form");
 const recentSearches = document.getElementById("recent-searches");
 const APIkey = "1ca1a82fc99c6fa9c9cf0d400c675270";
-var city = document.getElementById("search-input").value;
-var queryURL = `api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt={5}&appid=${APIkey} `;
+var city = document.getElementById("search-input");
+var queryURL = `api.openweathermap.org/data/2.5/forecast/daily?q=${city.value}&cnt={5}&appid=${APIkey} `;
 
 //function searchHandler(event) {
 //event.preventDefault();
@@ -11,11 +11,9 @@ var queryURL = `api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt={5}
 //console.error("Search value is required");
 
 function getWeather(event) {
-  fetch(queryURL).then((response) => {
-    console.log(response);
-    //return response.json();
-  });
-  //.then((JSONresponse) => {});
+  fetch(queryURL)
+    .then((res) => res.json())
+    .then((jsonData) => console.log(data));
 }
 
-searchForm.addEventListener("submit", getWeather);
+searchBtn.addEventListener("click", getWeather);
