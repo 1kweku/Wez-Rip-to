844434a1b20c1bbb8acc9d5eb22ecbfee44ef2fc
1kweku <kwekuplusp@gmail.com>
2022-12-15 21:47:10 -0500

    lat and lon functional

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const searchBtn = document.getElementById("search-btn");
 //const searchForm = document.getElementById("search-form");
 const recentSearches = document.getElementById("recent-searches");
 const APIkey = "1ca1a82fc99c6fa9c9cf0d400c675270";
-var city = document.getElementById("search-input");
+var city = document.getElementById("search-input").value;
 var test = "london";
-var geoQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${test}&appid=${APIkey}`;
+var geoQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIkey}`;
 //var weatherURL = `api.openweathermap.org/data/2.5/forecast/daily?q=london&cnt={5}&appid=${APIkey} `;
 
 //function searchHandler(event) {
@@ -16,13 +16,32 @@ function getCity(event) {
   fetch(geoQuery)
     .then((res) => res.json())
     .then((jsonData) => {
+      console.log(jsonData);
       var lat = jsonData[0].lat;
       var lon = jsonData[0].lon;
 
-      var weatherURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+      var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
 
-      fetch(weatherURL).then((response) => console.log(response));
+      fetch(weatherURL)
+        .then((weatherData) => weatherData.json())
+        .then((jsonWeather) => {
+          console.log(jsonWeather);
+          getWeather(jsonWeather);
+        });
     });
 }
+
+// let cityName = document.getElementById("city-name");
+// let cityTemp = document.getElementById("city-temp");
+// let cityWind = document.getElementById("city-wind");
+// let cityHumidity = document.getElementById("city-humidity");
+
+// function getWeather(cityWeather) {
+//   cityName.innerHTML = city;
+//   cityTemp.innerHTML = cityWeather.list.main.temp;
+//   cityWind.innerHTML = cityWeather.list.wind.speed;
+//   cityHumidity.innerHTML = cityWeather.list.main.humidity;
+//   console.log(cityWeather);
+// }
 
 searchBtn.addEventListener("click", getCity);
